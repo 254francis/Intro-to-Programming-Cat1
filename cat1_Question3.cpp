d0859c9ca303738b1257330db2bef81db79cd610
254francis <86851202+254francis@users.noreply.github.com>
2022-11-03 19:46:48 +0300

    Add files via upload

---
+++ cat1_Question3.cpp
@@ -0,0 +1,16 @@
+//Question3
+#include <iostream>
+using namespace std;
+int main()
+{
+//variable declarations
+int paye;
+int gross_salary;
+int net salary;
+//initialization
+paye =25%;
+gross_salary =10000;
+
+
+
+}
